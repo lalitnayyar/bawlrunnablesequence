b617b51f0589db110d776e0c35a519efe6649a47
Lalit <lalitnayyar@gmail.com>
2025-01-01 06:19:59 +0400

    grammar template and runnablepassthrough

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { PromptTemplate } from 'langchain/prompts'
 import { StringOutputParser } from 'langchain/schema/output_parser'
-import { RunnableSequence } from "langchain/schema/runnable"
+import { RunnableSequence, RunnablePassthrough } from "langchain/schema/runnable"
 
 import dotenv from 'dotenv'
 
@@ -22,12 +22,26 @@ const grammarTemplate = `Given a sentence correct the grammar.
     `
 const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate)
 
+const translationTemplate = `Given a sentence, translate that sentence into {language}
+    sentence: {grammatically_correct_sentence}
+    translated sentence:
+    `
+const translationPrompt = PromptTemplate.fromTemplate(translationTemplate)
+
 const punctuationChain = RunnableSequence.from([punctuationPrompt, llm, new StringOutputParser()])
 const grammarChain = RunnableSequence.from([grammarPrompt, llm, new StringOutputParser()])
+const translationChain = RunnableSequence.from([translationPrompt, llm, new StringOutputParser()])
 
 const chain = RunnableSequence.from([
-    { punctuated_sentence: punctuationChain },
-    grammarChain
+    {
+        punctuated_sentence: punctuationChain,
+        original_input: new RunnablePassthrough()
+    },
+    {
+        grammatically_correct_sentence: grammarChain,
+        original_input: original_input => console.log(original_input)
+    },
+    translationChain
 ])
 
 
